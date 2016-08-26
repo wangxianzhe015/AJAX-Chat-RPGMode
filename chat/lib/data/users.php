29f1a334e5868cabf87cbcce04912050511fe7c2
BanjoFox <pipnflinx@gmail.com>
2016-08-26 12:40:55 -0400

    Revert users.php
    Need to ignore the users.php file when I rsync from the dev server -.-

--- chat/lib/data/users.php
+++ chat/lib/data/users.php
@@ -24,54 +24,12 @@ $users[1]['userName'] = 'admin';
 $users[1]['password'] = 'admin';
 $users[1]['channels'] = array(0,1);
 
-// Superuser admin:
-$users[2] = array();
-$users[2]['userRole'] = AJAX_CHAT_ADMIN;
-$users[2]['userName'] = 'superadmin';
-$users[2]['password'] = 'superadmin';
-$users[2]['channels'] = array(0,1);
-
-// Poobah admin:
-$users[3] = array();
-$users[3]['userRole'] = AJAX_CHAT_ADMIN;
-$users[3]['userName'] = 'poobah_admin';
-$users[3]['password'] = 'poobah';
-$users[3]['channels'] = array(0,1);
-
 // Sample moderator user:
 $users[4] = array();
 $users[4]['userRole'] = AJAX_CHAT_MODERATOR;
 $users[4]['userName'] = 'moderator';
 $users[4]['password'] = 'moderator';
 $users[4]['channels'] = array(0,1);
-
-// Superuser moderator user:
-$users[5] = array();
-$users[5]['userRole'] = AJAX_CHAT_MODERATOR;
-$users[5]['userName'] = 'supermoderator';
-$users[5]['password'] = 'supermoderator';
-$users[5]['channels'] = array(0,1);
-
-// Poobah moderator user:
-$users[6] = array();
-$users[6]['userRole'] = AJAX_CHAT_MODERATOR;
-$users[6]['userName'] = 'poobah_moderator';
-$users[6]['password'] = 'poobah';
-$users[6]['channels'] = array(0,1);
-
-// Superuser registered user:
-$users[7] = array();
-$users[7]['userRole'] = AJAX_CHAT_USER;
-$users[7]['userName'] = 'superuser';
-$users[7]['password'] = 'superuser';
-$users[7]['channels'] = array(0,1);
-
-// Poobah registered user:
-$users[8] = array();
-$users[8]['userRole'] = AJAX_CHAT_USER;
-$users[8]['userName'] = 'poobah_user';
-$users[8]['password'] = 'poobah';
-$users[8]['channels'] = array(0,1);
 
 // Sample registered user:
 $users[9] = array();
