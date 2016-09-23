1405efdf235ced9a936bbee2d29b0d36b66bf7c2
admin <admin@zappix.com>
2016-09-23 14:25:04 -0400

    More updates

--- chat/js/custom.js
+++ chat/js/custom.js
@@ -14,3 +14,19 @@ ajaxChat.replaceCustomCommands = function(text, textParts) {
 	return text;
 }
  */
+
+// Announce Channel Information
+// Return true to override built-in info message processing, return false to use your override and skip the built in handler
+ajaxChat.handleCustomInfoMessage = function(infoType, infoData) {
+	switch(infoType) {
+		case 'channelID':
+			this.channelID = infoData;
+			if (this.channelID == 0)
+				this.addChatBotMessageToChatList('Welcome to the Public room. Please follow the rules.');
+			if (this.channelID == 1)
+				this.addChatBotMessageToChatList('Welcome to Private room. Rules are for... people that follow rules.');
+			return true;
+		default:
+			return false;
+	}
+}
